4e3a5717938f3a4d58bac21e064beaefd3791a15
Fakenhit <mutiev20@yandex.ru>
2025-02-01 17:48:27 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,45 @@
+let mouse = 1;
+function get_mouse(model) {
+    window.alert(`
+       ____()()
+      /      @@
+'~~~~~\_;m__m._>o    jgs
+`);
+function othet(model) {
+if (mouse=true){
+console.log(`${model}мышь успех`);
+}
+}
+othet(model);
+}
+
+get_mouse("model#1");
+
+
+
+function printPerson(person) {
+    console.log("Name:", person[0]);
+    console.log("Age:", person[1]);
+    console.log("Email:", person[2]);
+    console.log("=========================");
+}
+const tom = ["Tom", 39, "tom@example.com"]; 
+const bob = ["Bob", 43, "bob@example.com"]; 
+ 
+printPerson(tom);
+printPerson(bob);
+
+
+function sum() {
+    console.log(arguments);
+}
+
+sum(1,2,4);
+sum("bdhi",4,9);
+
+function mous() {
+    return arguments[0] + arguments[1];
+}
+ let mo = mous(3,5);
+ document.write(mo);
+ 
